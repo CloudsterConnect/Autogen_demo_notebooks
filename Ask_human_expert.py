3fe25a7578d9307b045eeccf2c14ef424826272b
haniehba <haniehb@cloudsterconnect.co>
2024-07-08 19:00:45 +1000

    human expert code

---
+++ Ask_human_expert.py
@@ -0,0 +1,190 @@
+
+#!pip install pyautogen==0.2.27
+import autogen
+from dotenv import load_dotenv
+import os
+from typing import Annotated
+from autogen.agentchat.contrib.gpt_assistant_agent import GPTAssistantAgent
+
+#Variables
+
+# %%
+load_dotenv()
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+AUTOGEN_USE_DOCKER=False
+DEFAULT_PATH="./tmp/"
+
+
+#LLM Configurations
+
+config_list = [
+    {
+        "model": "gpt-4o",
+        "base_url": "https://api.openai.com/v1",
+        "api_type": "openai",
+        "api_key": OPENAI_API_KEY,
+        "tags": ["gpt4", "openai"]
+    },
+]
+
+llm_config = {
+    "temperature": 0,
+    "config_list": config_list,
+    "timeout": 120,
+}
+
+
+#Functions
+
+def is_termination_msg(content) -> bool:
+    have_content = content.get("content", None) is not None
+    if have_content and "TERMINATE" in content["content"]:
+        return True
+    return False
+
+def read_file(dir,filename):
+    try:
+        task_file_path = os.path.join(dir, filename)
+        with open(task_file_path, 'r',encoding='utf-8') as file:
+            content = file.read()
+        return content
+    except Exception as e:
+        return f"An error occurred while reading the file: {e}"
+
+#Agents
+
+user_proxy = autogen.UserProxyAgent(
+    name="user_proxy",# The name is flexible, but should not contain spaces to work in group chat.
+    human_input_mode='NEVER',#['ALWAYS', 'TERMINATE', 'NEVER']
+    is_termination_msg=is_termination_msg,
+    code_execution_config={
+        "last_n_messages": 1,
+        "work_dir": DEFAULT_PATH,
+        "use_docker": False,
+    }
+)
+
+gpt_assistant = GPTAssistantAgent(
+    name="Coder Assistant",
+    llm_config={
+        "config_list": config_list,
+    },
+    assistant_config={
+        "tools": [{"type": "code_interpreter"}],
+    },
+    instructions="You are an expert at writing code to solve problems. Reply TERMINATE when the question is answered.",
+)
+
+engineer = autogen.AssistantAgent(
+    name="Engineer",# The name is flexible, but should not contain spaces to work in group chat.
+    is_termination_msg=is_termination_msg,
+    human_input_mode= 'NEVER',#['ALWAYS', 'TERMINATE', 'NEVER']
+    llm_config=llm_config,
+    system_message="""Engineer. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
+    If the error can't be fixed or if the task is not solved even after the code is executed successfully, ask human expert for help.
+    """,
+    description="An expert coder who writes and tests code in small increments, ensuring Docker runs successfully and the code is fully functional. Provides clear, executable code blocks and updates requirements.txt, Dockerfile, and README.md instructions accordingly."
+)
+
+
+#Skills
+
+@engineer.register_for_llm(description="Ask question from the human expert")
+@user_proxy.register_for_execution()
+def ask_human_expert(question: Annotated[str, "The question you want to ask the human expert."]) -> Annotated[str, "Answer"]:
+    # Path to the sound file
+    answer = input(f"Please answer the question: {question}\n")
+    return answer
+
+
+
+@engineer.register_for_llm(description="Ask question about a file from the human expert")
+@user_proxy.register_for_execution()
+def ask_human_expert_about_code(question: Annotated[str, "The question you want to ask the human expert."],
+                     filename: Annotated[str, "Name and path of file related to the question."]) -> Annotated[str, "Answer"]:
+    """
+    Answer a technical question related to a file.
+    """
+    file_path = os.path.join(DEFAULT_PATH, filename)
+    try:
+        with open(file_path, "r", encoding='utf-8') as file:
+            contents = file.read()
+    except Exception as e:
+        return f"An error occurred while reading the file: {e}"
+    
+    result =user_proxy.initiate_chat(
+        gpt_assistant,
+        message=f"Please answer the question: {question}\n{contents}",
+        is_termination_msg=lambda msg: "TERMINATE" in msg["content"],
+        human_input_mode="NEVER",
+        clear_history=True,
+        max_consecutive_auto_reply=0,
+        summary_method= "last_msg",
+        code_execution_config={"work_dir": DEFAULT_PATH},
+ 
+    )
+    
+    answer = result.summary
+    return answer
+
+@user_proxy.register_for_execution()
+@engineer.register_for_llm(description="To install new Python modules using pip.")
+def pip_install(
+    module_name: Annotated[str, "The name of the module to install."], 
+    shell_command: Annotated[str, "The shell command to install the module using pip."]
+):
+    try:
+        # Construct the pip install command
+        pip_command = f"pip install {module_name}"
+
+        # Use the default path if provided
+        if DEFAULT_PATH:
+            pip_command = f"cd {DEFAULT_PATH} && {pip_command}"
+
+        # Execute the pip install command
+        run_result = subprocess.run(shell_command or pip_command, shell=True, capture_output=True, text=True)
+
+        if run_result.returncode == 0:
+            return 0, f"Successful:\n{run_result.stdout}"
+        else:
+            return run_result.returncode, f"Failed:\n{run_result.stderr}"
+    except Exception as e:
+        return 1, str(e)
+
+
+#create_file_with_code: Creates a new file with the given code.
+@user_proxy.register_for_execution()
+@engineer.register_for_llm(description="Create a new file with code.")
+def create_file_with_code(
+    filename: Annotated[str, "Name and path of file to create."], code: Annotated[str, "Code to write in the file."]
+):
+    with open(DEFAULT_PATH +'/'+ filename, "w") as file:
+        file.write(code)
+    return 0, "File created successfully"
+
+
+
+#Chat
+
+chat_result = autogen.initiate_chats(
+    [
+        
+        {
+            "recipient": engineer,
+            "sender": user_proxy,
+            "message": "Create a flask app with login page.",
+            "max_turns": 20, #rounds between user proxy and engineer
+            "max_consecutive_auto_reply":2,
+            "clear_history": True,
+            "silent": False,
+            "summary_method": "last_msg",
+            "work_dir": DEFAULT_PATH ,
+        },
+        
+    ]
+) 
+
+#Delete the assistant
+gpt_assistant.delete_assistant()
+
+
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Autogen_demo_notebooks
 CloudsterConnect AUTOGEN demo notebooks
+
+
+### create a virtual environment named 'autogen' for Python on Windows
+py or python -m venv autogenenv
+# After running the command, you should see a new directory named 'autogen' in your current directory. 
+# To activate the virtual environment
+autogenenv\Scripts\activate
+
+
+# Install 
+pip install pyautogen==0.2.27
+pip install openai==1.30.3
+pip install packaging
+# test the code
+py or python Ask_human_expert.py
+
+
+# to deactivate the virtual environment 
+deactivate
